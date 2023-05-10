c4cdb765f9c68be6b0e9438a17eaf37bed0b1cf2
Milad <mi.alkhamis@pm.me>
2023-05-10 15:02:31 +0000

    Just run terraform fmt -recursive :)

--- 1-Terraform-k8s/outputs.tf
+++ 1-Terraform-k8s/outputs.tf
@@ -1,4 +1,4 @@
-output ServicePort {
+output "ServicePort" {
   value       = "Nodport is ${kubernetes_service.nginx.spec.0.port.0.node_port}"
   sensitive   = true
   description = "show node port"
--- 2-Terraform-module-docker/main.tf
+++ 2-Terraform-module-docker/main.tf
@@ -1,9 +1,9 @@
 module "nginx" {
-  source                    = "./nginx"
-  nginx_version =    "1.19"
-  deployment_name         = "my_naginx"
-  common_name =   "milad.local"
-  dns_name = ["nginx.local","www.nginx.local"]
+  source          = "./nginx"
+  nginx_version   = "1.19"
+  deployment_name = "my_naginx"
+  common_name     = "milad.local"
+  dns_name        = ["nginx.local", "www.nginx.local"]
 }
 
 output "nginx_output" {
