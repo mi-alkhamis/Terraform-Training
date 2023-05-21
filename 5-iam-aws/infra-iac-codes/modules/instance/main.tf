c190d908f4f4075a32b8fcaee97206ddafeffbea
Milad <mi.alkhamis@pm.me>
2023-05-21 19:03:42 +0000

    replace `vpc_security_group_ids` to fix recreation EC2

--- 5-iam-aws/infra-iac-codes/modules/instance/main.tf
+++ 5-iam-aws/infra-iac-codes/modules/instance/main.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "my_instance" {
   instance_type               = var.instance_type
   key_name                    = var.instance_key_name
   associate_public_ip_address = var.instance_associate_public_ip_address
-  security_groups             = var.instance_security_groups
+  # security_groups             = var.instance_security_groups
+  vpc_security_group_ids = var.instance_security_groups
   subnet_id                   = var.instance_subnet_id
 
   tags = {
