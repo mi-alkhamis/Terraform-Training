4cf0f9f7333edb536aa08af983b1cbb6534e1400
Milad <mi.alkhamis@pm.me>
2023-05-21 18:01:04 +0000

    Instance Module

---
+++ 5-iam-aws/infra-iac-codes/modules/instance/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "my_instance" {
+  ami                         = var.instance_ami
+  instance_type               = var.instance_type
+  key_name                    = var.instance_key_name
+  associate_public_ip_address = var.instance_associate_public_ip_address
+  security_groups             = var.instance_security_groups
+  subnet_id                   = var.instance_subnet_id
+
+  tags = {
+    "name" = var.instance_name
+  }
+
+}
---
+++ 5-iam-aws/infra-iac-codes/modules/instance/outputs.tf
@@ -0,0 +1,9 @@
+output "instance_private_ip" {
+  value = aws_instance.my_instance.private_ip
+
+}
+
+output "instance_public_ip" {
+  value = aws_instance.my_instance.public_ip
+
+}
